ba9adf97562b8641e12f4e80d12e11bc2493bf82
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2012-12-08 21:57:21 +0100

    zmiana wartosci limitu

--- firewall/firewall_syn_flood.sh
+++ firewall/firewall_syn_flood.sh
@@ -5,7 +5,7 @@ iptables -A SYN_FLOOD_DROPLOG -m hashlimit --hashlimit-upto 1/minute --hashlimit
 iptables -A SYN_FLOOD_DROPLOG -j REJECT
 
 iptables -N SYN_FLOOD
-iptables -A SYN_FLOOD -m hashlimit --hashlimit-above 5/second --hashlimit-mode srcip \
+iptables -A SYN_FLOOD -m hashlimit --hashlimit-above 1/second --hashlimit-mode srcip \
 	--hashlimit-name syn_flood --hashlimit-burst 5 -j SYN_FLOOD_DROPLOG
 
 iptables -A IPS -p tcp --tcp-flags ALL SYN -j SYN_FLOOD
