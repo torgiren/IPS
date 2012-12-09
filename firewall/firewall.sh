857fe799b9d3af1f573e942064b02dacbff120f8
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2012-12-09 03:31:13 +0100

    firewalle

--- firewall/firewall.sh
+++ firewall/firewall.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
+echo "Clean..."
 ./firewall_clean.sh
 iptables -N IPS
 
+echo "SSH Brute Force"
 ./firewall_brute.sh
+echo "SYN Flood"
 ./firewall_syn_flood.sh
+echo "Timestamp Request"
+./firewall_timestamp_request.sh
 iptables -A INPUT -m state --state NEW -j IPS
