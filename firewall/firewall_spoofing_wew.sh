cbe4eccfccac0d77a625ee431ac9ed379517c6f3
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2012-12-17 04:42:50 +0100

    poprawa nazwy łańcucha

--- firewall/firewall_spoofing_wew.sh
+++ firewall/firewall_spoofing_wew.sh
@@ -7,7 +7,7 @@ ipset -A clients 192.168.242.111%00:26:b6:66:d5:18
 iptables -N WEW_SPOOF_DROPLOG
 iptables -A WEW_SPOOF_DROPLOG -m hashlimit --hashlimit-upto 1/minute --hashlimit-name wew_spoof_log \
 	--hashlimit-burst 1 -j LOG --log-prefix "WEW_SPOOF"
-iptables -A SSH_BRUTE_DROPLOG -j REJECT
+iptables -A WEW_SPOOF_DROPLOG -j REJECT
 
 
 iptables -A IPS -i eth0 -m set ! --match-set clients src -J WEW_SPOOF_DROPLOG
