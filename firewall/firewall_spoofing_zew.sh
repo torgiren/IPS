4557c710f62f29083f710434576dda1e8bbfedaa
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2012-12-19 22:00:40 +0100

    firewall

---
+++ firewall/firewall_spoofing_zew.sh
@@ -0,0 +1,12 @@
+iptables -N ZEW_SPOOF_DROPLOG
+iptables -A ZEW_SPOOF_DROPLOG -m hashlimit --hashlimit-upto 1/minute --hashlimit-name wew_spoof_log \
+	--hashlimit-burst 1 -j LOG --log-prefix "ZEW_SPOOF"
+iptables -A ZEW_SPOOF_DROPLOG -j REJECT
+
+iptables -N ZEW_SPOOF
+iptables -A ZEW_SPOOF -s 192.168.0.0/16 -j ZEW_SPOOF_DROPLOG
+iptables -A ZEW_SPOOF -s 172.16.0.0/12 -j ZEW_SPOOF_DROPLOG
+iptables -A ZEW_SPOOF -s 127.0.0.0/8 -j ZEW_SPOOF_DROPLOG
+iptables -A ZEW_SPOOF -s 10.0.0.0/8 -j ZEW_SPOOF_DROPLOG
+
+iptables -A IPS -i eth1 -J ZEW_SPOOF
