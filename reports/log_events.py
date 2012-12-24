4d932c6e586c180e55bce938face586154c960a8
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2012-12-24 01:12:03 +0100

    closes #19. dodawanie logow do bazy

--- reports/log_events.py
+++ reports/log_events.py
@@ -9,12 +9,32 @@ setup_environ(settings)
 import datetime
 
 t=datetime.datetime.now()-datetime.timedelta(minutes=1)
-#d=datetime.datetime.astimezone(datetime.tzinfo('Europe/Warsaw'))
-#print d.datetime.now()
-czas="%d %d:%d:"%(t.day,t.hour,t.minute)
+#t=datetime.datetime.now()-datetime.timedelta(minutes=1)
+czas="%d %02d:%02d:"%(t.day,t.hour,t.minute)
 plik=open("/var/log/ips.log")
 lines=plik.readlines()
 for line in lines:
 	if czas in line:
-		print line
-		print "Linia pasuje"
+		data=line.split()
+		logczas="%d-%02d-%02d %s"%(t.year,t.month,t.day,data[2])
+		data=data[7:]
+	#	data=line[line.find('IPS:')+5:].split()
+		typ=None
+		src=None
+		dst=None
+		sport=None
+		dport=None
+		proto=None
+		typ=data[0]
+		for d in data:
+			if d.startswith("SRC="):
+				src=d[4:]
+			elif d.startswith("DST="):
+				dst=d[4:]
+			elif d.startswith("DPT="):
+				dport=d[4:]
+			elif d.startswith("SPT="):
+				sport=d[4:]
+			elif d.startswith("PROTO="):
+				proto=d[6:]
+		Event(czas=logczas,typ=typ,src=src,dst=dst,sport=sport,dport=dport,proto=proto,log=line).save()
--- reports/logs/models.py
+++ reports/logs/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
-class Type(models.Model):
-	nazwa=models.CharField(max_length=30)
+#class Type(models.Model):
+#	nazwa=models.CharField(max_length=30,unique=True)
 class Event(models.Model):
-	czas=models.DateField()
-	typ=models.ForeignKey(Type)
+	czas=models.DateTimeField()
+	typ=models.CharField(max_length=30)
+	src=models.CharField(max_length=16)
+	dst=models.CharField(max_length=16)
+	sport=models.CharField(max_length=6)
+	dport=models.CharField(max_length=6)
+	proto=models.CharField(max_length=10)
+	log=models.CharField(max_length=500)
 
 
 # Create your models here.
